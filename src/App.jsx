33aac8ded2fbb03892b1c432dbaf3cce8adbb091
Nicolas <vazqueznicolasdev@gmail.com>
2022-08-23 13:50:01 -0300

    Project started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Modal } from "./components/Modal/Modal";
+
+function App() {
+	return (
+		<div className="container">
+			<button>Open Modal</button>
+			<Modal />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./modal.module.css";
+
+export const Modal = () => {
+	return (
+		<div className={styles.modalBack}>
+			<div className={styles.modalContainer}>
+				<div>
+					<h2>Modal</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nisi. Dolorem est
+						esse iste perferendis.
+					</p>
+				</div>
+				<button className={styles.modal__closeBtn}>Close</button>
+			</div>
+		</div>
+	);
+};
